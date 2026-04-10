e9024c8baa5165711637eb8de47a82b956a29fb9
Cybersheepdog <jmwiedner@gmail.com>
2026-04-10 09:46:39 -0400

    Initial Commit.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Adversary-Hunter
+# Adversary Hunter
+[![Build Status](https://img.shields.io/badge/platform-Windows%20%7C%20Linux-blue.svg)](https://shields.io)
+![Maintenance](https://img.shields.io/maintenance/yes/2026.svg?style=flat-square)
+[![Github last commit](https://img.shields.io/github/last-commit/cybersheepdog/Adversary-Hunterer.svg?style=flat-square)]
+
+Adversary Hunter is a free-to-use-community-driven IOC feed that uses [Shodan](https://www.shodan.io/) and [Censys](https://search.censys.io/) searches to collect IP addresses of known malware/botnet/C2 infrastructure.
+
+## Honorable Mentions
+
+This is based on [C2-Tracker](https://github.com/montysecurity/C2-Tracker) by [@montysecurity](https://github.com/montysecurity).
+
+## Usage
+
+The most recent collection will be stored in `data/`. The IPs are seperated by the name of the tool and there is an `all.txt` that contains all of the IPs. As it currently stands this feed updates `weekly` on Monday.
+
+The queries for Censys and Shodan are in the code in the respective function and you can see them there.  I am also including a feature to define custom queries in a text file that will be imported into the program and added to the pre-defined queries.  This gives intel and research teams the ability to define queries they may not want to be publically available.
+
+### Investigations/Historical Analysis
+
+- The repo, by its nature, has version control. This means you can search the history of the repo for when an IP was present in the results. 
+- I have created a python script to ingest this data into OpenCTI [here](https://github.com/cybersheepdog/opencti-c2-tracker)
+
+## What do I track?
+
+- C2's
+    - TBD
+- Malware
+    - Vidar Stealer
+- Tools
+    - TBD 
+- Botnets
+    - TBD
+
+## Running Locally
+
+If you want to host a private version, put your Shodan API key in an environment variable called `SHODAN_API_KEY`, and setup your Censys credentials in `CENSYS_API_ID` & `CENSYS_API_SECRET`
+
+```bash
+python3 -m pip install -r requirements.txt
+python3 hunter.py
+```
+
+## Contributing
+
+I encourage opening an issue/PR if you know of any additional Shodan/Censys searches for identifying adversary infrastructure. I will not set any hard guidelines around what can be submitted, just know, **fidelity is paramount** (high true/false positive ratio is the focus).
+
---
+++ hunter.py
@@ -0,0 +1,131 @@
+# Python Standard Library Imports
+import json
+import os
+
+# Third Party Imports
+from dotenv import load_dotenv
+from shodan import Shodan, exception
+from censys.search import CensysHosts
+
+def shodan():
+    api_key = os.environ["SHODAN_API_KEY"].strip()
+    api = Shodan(api_key)
+    queries = {
+        "Cobalt Strike C2": [
+            "ssl.cert.serial:146473198",
+            "hash:-2007783223 port:50050",
+            "product:'Cobalt Strike Beacon'",
+            "ssl:foren.zik"
+        ]
+    }
+
+    # Try and load custom queries from shodan_queries.json
+    try:
+        with open('shodan_queries.json', 'r') as file:
+            custom_shodan_queries = json.load(file)
+
+        # Merge the dictionaries of queries together
+        queries = queries | custom_shodan_queries
+    except:
+        print("Either the shodan_queries.json file is empty or there is an error in the format.")
+
+    # TODO: Check for duplicate queries to avoid unncessary api calls.
+    # TODO: Check for differnt queries for the same tool and merge into one.
+
+    # https://www.techiedelight.com/delete-all-files-directory-python/
+    dir_to_clean = "data"
+    for file in os.scandir(dir_to_clean):
+        os.remove(file.path)
+
+    ip_set_from_all_products = set()
+    for product in queries:
+        ip_set_from_product = set()
+        product_ips_file = open(f"data/{product} IPs.txt", "a")
+        for query in queries[product]:
+            print(f"Product: {product}, Query: {query}")
+            results = api.search_cursor(query)
+            # Catch Shodan Query Errors and pass onto the next C2
+            # TODO: make it restart main() while keeping track of what was already documented
+            try:
+                for result in results:
+                    ip = str(result["ip_str"])
+                    ip_set_from_product.add(ip)
+                    ip_set_from_all_products.add(ip)
+            except exception.APIError:
+                continue
+        for ip in ip_set_from_product:
+            product_ips_file.write(f"{ip}\n")
+
+    all_ips_file = open("data/all.txt", "a")
+    for ip in ip_set_from_all_products:
+        all_ips_file.write(f"{ip}\n")
+
+def censys():
+    queries = {
+        "Poseidon C2": [
+            "host.services.endpoints.http.html_title='POSEIDON'",
+            "web.endpoints.http.html_title='POSEIDON'"
+        ]
+    }
+
+    # Try and load custom queries from censys_queries.json
+    try:
+        with open('censys_queries.json', 'r') as file:
+            custom_censys_queries = json.load(file)
+
+        # Merge the dictionaires of queries together
+        queries = queries | custom_censys_queries
+    except:
+        print("Either the censys_queries.json file is empty or there is an error in the format.")
+
+    # TODO: Check for duplicate queries to avoid unnecessary api calls.
+    # TODO: Check for different queries for the same tool and merge into one.
+
+    h = CensysHosts()
+    all_ips = set()
+    for product in queries:
+        ips = set()
+        product_ips_file = open(f"data/{product} IPs.txt", "a")
+        for search_string in queries[product]:
+            print(f"Product: {product}, Query: {search_string}")
+            query = h.search(search_string)
+            results = None
+            try:
+                results = query()
+            except Exception as err:
+                print(err)
+                continue
+            for host in results:
+                ip = str(host['ip'])
+                all_ips.add(ip)
+                ips.add(ip)
+        for ip in ips:
+            product_ips_file.write(f"{ip}\n")
+    all_ips_file = open("data/all.txt", "a")
+    for ip in all_ips:
+        all_ips_file.write(f"{ip}\n")
+
+def deconflict():
+    # Remove any duplicates from the files
+    files = os.listdir("data/")
+    for file in files:
+        filepath = f"data/{file}"
+        f = open(filepath, "r")
+        lines = f.readlines()
+        f.close()
+        if len(lines) != len(set(lines)):
+            print(f"Deconflicting: {filepath}")
+            os.remove(filepath)
+            f = open(filepath, "a")
+            for line in set(lines):
+                f.write(line)
+            f.close()
+
+def main():
+    load_dotenv()
+    shodan()
+    censys()
+    deconflict()
+
+if __name__ == '__main__':
+    main()
